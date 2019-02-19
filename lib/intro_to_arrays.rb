9312619a24e3424dd44c0c0c0936cd94e78c0abb
James Cox <jamesncox@yahoo.com>
2019-02-19 17:55:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,9 +7,7 @@ def array_with_two_elements
   @my_new_array = ["a", 1]
 end
 
-def indexing
-  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-end
+@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
 
 def first_element
   @taylor_swift[0]
