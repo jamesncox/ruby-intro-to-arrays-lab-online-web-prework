3d3a9853660d4be2e5a99529be21e5f189c335a3
James Cox <jamesncox@yahoo.com>
2019-02-19 17:47:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,7 +7,7 @@ def array_with_two_elements
   @my_new_array = ["a", 1]
 end
 
-def first_element(@taylor_swift)
+def first_element
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   my_first_element = @taylor_swift.shift
 end
