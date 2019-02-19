76b474d00287c48a4891d32d87c8ccdc12701be2
James Cox <jamesncox@yahoo.com>
2019-02-19 18:06:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -23,6 +23,6 @@ end
 
 @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
 
-def first_element_with_array_methods(first country)
+def first_element_with_array_methods(first_country)
   @south_east_asia.fetch[0]
 end
